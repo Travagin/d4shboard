10d4cc747dcfe14b71daba36a07f283413a72991
Manu <imacnu@gmail.com>
2015-03-09 09:37:36 +0100

    Array push

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,37 +1,44 @@
 'use strict';
 
-angular.module('github')
-.controller('MainCtrl', function ($scope,$http) {
+var app = angular.module('github');
+
+app.controller('MainCtrl',['$scope','$http',function ($scope,$http) {
+
+	$scope.widget = {};
+	$scope.widgets = [];
+	$scope.widgetConfiguration = [];
 
 	$scope.addWidget = function(widget){
 
-		$scope.widgetConfiguration = [{color:'',size:''}];
-		$scope.widgetConfiguration = angular.copy(widget);
+
+		$scope.widgets = $scope.widgetConfiguration.push(angular.copy(widget));
+		// $scope.widget = {};
 
 		$scope.widgetAdded = true;
 
 		console.log("Add Widget ok");
-		console.log($scope.widgetConfiguration);
-
-		$scope.search = function(username){
-
-			console.log("Searching for " + username);
-
-			$scope.userNotFound = false;
-			$scope.loaded = false;
-
-			$http.get("https://api.github.com/users/" + username)
-			.success(function (data) {
-
-				$scope.user = data;
-				$scope.loaded = true;
-			})
-			.error(function () {
-				$scope.userNotFound = true;
-			});
-		}
+		console.log($scope.widgets);
 	};
-});
+
+	$scope.search = function(username){
+
+		console.log("Searching for " + username);
+
+		$scope.userNotFound = false;
+		$scope.loaded = false;
+
+		$http.get("https://api.github.com/users/" + username)
+		.success(function (data) {
+
+			$scope.user = data;
+			$scope.loaded = true;
+		})
+		.error(function () {
+			$scope.userNotFound = true;
+		});
+	}
+
+}]);
 
 
 
